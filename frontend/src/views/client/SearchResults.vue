aaa247c9e49e6b6acd270df114fc2a17f55f6b5c
le_géomaticien <98210478+ygouhean@users.noreply.github.com>
2025-10-29 05:32:39 +0000

    Change filter group background color to black

--- frontend/src/views/client/SearchResults.vue
+++ frontend/src/views/client/SearchResults.vue
@@ -299,7 +299,7 @@ export default {
 
 .filter-group select {
   padding: 0.5rem;
-  background-color: rgba(255, 255, 255, 0.1);
+  background-color: rgba(0, 0, 0, 0.1);
   border: 1px solid #333;
   border-radius: 4px;
   color: white;
